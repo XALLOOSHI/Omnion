b51f0be233284bf7eef30d11b66207c52c81e406
matthew dayananda <supersmashbrosmatthew@gmail.com>
2025-04-07 12:55:12 -0400

    TEXT!!!

--- code/main.py
+++ code/main.py
@@ -8,11 +8,12 @@ class Game:
         self.running, self.playing = True, False
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
         self.screen = pygame.display.set_mode((Width,Height))
+        self.display = pygame.Surface((Width,Height))
         pygame.display.set_caption('Omnion')
         self.clock = pygame.time.Clock()
         self.level = Level()
-        self.font_name = '8-BIT WONDER.TTF'
-        self.WHITE = (0,0,0)
+        self.font_name = None
+        self.WHITE = (255,255,255)
 
     def check_events(self):
         for event in pygame.event.get():
@@ -36,9 +37,10 @@ class Game:
             self.check_events()
             if self.START_KEY:
                 self.playing= False
-            self.screen.fill('black')
-            self.draw_text('Omnion', 20, Width/2, Height/2 )
-            self.screen.blit(self.screen, (0,0))
+            self.display.fill('black')
+            self.draw_text('Omnion', 20, Width/2, Height/2)
+            self.screen.blit(self.display, (0,0))
+            pygame.display.update()
             self.reset_keys() 
 
     def run(self):
@@ -48,19 +50,22 @@ class Game:
                  pygame.quit()
                  sys.exit()
             
-            self.screen.fill('black')
+            #self.screen.fill('black')
+            self.screen.blit(self.display, (0,0))
             self.level.run()
             pygame.display.update()
             self.clock.tick(Fps)
     
     def draw_text(self, text, size, x,y):
-        font = pygame.font.Font(self.font_name,size)
+        font = pygame.font.Font(self.font_name, size)
         text_surface = font.render(text, True, self.WHITE)
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
-        self.screen.blit(text_surface,text_rect)
+        self.display.blit(text_surface,text_rect)
 
 
 if __name__ == '__main__':
     game = Game()
+    game.playing = True 
+    game.game_loop()
     game.run()
